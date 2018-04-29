c5fcdd3f1cb1bd62cb178a4745032c1365907579
fangjy6 <fangjy6@gmail.com>
2018-04-29 10:58:00 +0800

    Delete recommend.py

--- recommend.py
+++
@@ -1,7 +0,0 @@
-import sys
-import RecommendationSys
-
-if __name__ == "__main__":
-    customer_id = sys.argv[1]
-    N = sys.argv[2]
-    print(RecommendationSys.getRecommendation(customer_id,N=N))
